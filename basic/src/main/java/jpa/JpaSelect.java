4c03ed0c87eb170aea178f1ac1719f70fdbdf042
주상현 <monayo.jsh@gmail.com>
2023-02-23 09:58:53 +0900

    JPA 영속성 컨테이너의 이점 - 엔티티 조회 시 1차 캐시 기능 제공  : 영속성 컨테이너에 존재하면 바로 응답 그렇지 않으면 DB 쿼리 수행

--- basic/src/main/java/jpa/JpaSelect.java
+++ basic/src/main/java/jpa/JpaSelect.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-public class JpaInsert {
+public class JpaSelect {
 
     public static void main(String[] args) {
         //EntityManagerFactory 하나만 생성해서 애플리케이션 전체에서 공유
@@ -21,17 +21,14 @@ public class JpaInsert {
         transaction.begin();
 
         try {
-            Member member = new Member();
-            member.setId(0L);
-            member.setName("member1");
 
-            System.out.println("before persist");
-            entityManager.persist(member);
-            System.out.println("after persist");
+            // 동일한 Select 2회 호출 시 1번의 Select
+            Member member1 = entityManager.find(Member.class, 0L);
+            Member member2 = entityManager.find(Member.class, 0L);
 
-            System.out.println("before transaction commit");
+            System.out.println("member1 == member2 : " + (member1 == member2));
+
             transaction.commit();
-            System.out.println("after transaction commit");
         } catch(Exception e) {
             e.printStackTrace();
             transaction.rollback();
