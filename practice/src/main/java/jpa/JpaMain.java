bb418d7285e4e6861e9783803f68979a48bcf8d1
주상현 <monayo.jsh@gmail.com>
2023-02-23 15:13:26 +0900

    jpa 학습을 위한 엔티티 및 기본 로직 추가

---
+++ practice/src/main/java/jpa/JpaMain.java
@@ -0,0 +1,34 @@
+package jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        //EntityManagerFactory 하나만 생성해서 애플리케이션 전체에서 공유
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+
+        //EntityManager 쓰레드간 공유 X 일회성으로 사용하고 버리기
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        //JPA 는 Transaction 안에서만 동작
+        EntityTransaction tx = entityManager.getTransaction();
+
+        tx.begin();
+        try {
+
+            tx.commit();
+        } catch(Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        }
+
+        entityManager.close();
+
+        entityManagerFactory.close();
+    }
+
+}
---
+++ practice/src/main/java/jpa/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package jpa.domain;
+
+public enum OrderStatus {
+    ORDER, CACNEL
+}
