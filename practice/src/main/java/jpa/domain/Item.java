3269bd900a6e54ee40f910ea72de18395a9819fe
주상현 <monayo.jsh@gmail.com>
2023-02-24 10:02:38 +0900

    연관 관계 매핑 실습  - 주문 객체에 배송 객체 참조 추가  - 아이템 객체와 카테고리 객체 참조 추가

--- practice/src/main/java/jpa/domain/Item.java
+++ practice/src/main/java/jpa/domain/Item.java
@@ -1,9 +1,12 @@
 package jpa.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Item {
@@ -15,6 +18,9 @@ public class Item {
     private String name;
     private Integer price;
     private Integer stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
     public Long getId() {
         return id;
--- practice/src/main/java/jpa/domain/Order.java
+++ practice/src/main/java/jpa/domain/Order.java
@@ -1,6 +1,8 @@
 package jpa.domain;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -9,6 +11,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +26,13 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
+
+    @OneToOne
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate;
 
@@ -42,6 +53,14 @@ public class Order {
 
     public void setMember(Member member) {
         this.member = member;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
     }
 
     public LocalDateTime getOrderDate() {
