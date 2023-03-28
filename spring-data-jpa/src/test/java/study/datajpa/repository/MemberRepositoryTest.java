ba1ab6864c3eb97ed78fde076802795fa2a0b16d
주상현 <monayo.jsh@gmail.com>
2023-03-28 13:52:36 +0900

    spring-data-jpa Query Dto 학습

--- spring-data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ spring-data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -9,12 +9,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 @SpringBootTest
 @Transactional
 @Rollback(value = false)
 class MemberRepositoryTest {
+
+    @Autowired
+    TeamRepository teamRepository;
 
     @Autowired
     MemberRepository memberRepository;
@@ -97,5 +102,46 @@ class MemberRepositoryTest {
         for(Member member : members) {
             assertThat(member.getUsername()).isEqualTo(username);
         }
+    }
+
+
+    @Test
+    void testQueryVar() {
+
+        Member memberA = new Member("memberA", 10);
+        Member memberB = new Member("memberB", 20);
+        Member memberC = new Member("memberC", 30);
+
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+        memberRepository.save(memberC);
+
+        List<String> members = memberRepository.findUsernameList();
+
+        assertThat(members.size()).isEqualTo(3);
+    }
+
+    @Test
+    void testQueryDto() {
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        Team teamC = new Team("teamC");
+
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        teamRepository.save(teamC);
+
+        Member memberA = new Member("memberA", 10, teamA);
+        Member memberB = new Member("memberB", 20, teamB);
+        Member memberC = new Member("memberC", 30, teamC);
+
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+        memberRepository.save(memberC);
+
+        List<MemberDto> members = memberRepository.findMemberDto();
+
+        assertThat(members.size()).isEqualTo(3);
     }
 }
